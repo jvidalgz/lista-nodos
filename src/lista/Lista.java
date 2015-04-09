17a84d2c8c54ea51dff06111f88bfefd7df4230c
jaime <jvidalgz@gmail.com>
2015-04-09 15:27:32 -0300

    agrega atributos y metodo add

--- src/lista/Lista.java
+++ src/lista/Lista.java
@@ -4,4 +4,23 @@ package lista;
  * Created by jaime on 09-04-2015.
  */
 public class Lista {
+    // instance variables
+    public static final int CAPACITY = 16; // default array capacity
+    private Nodo[] data; // generic array used for storage
+    private int size = 0; // current number of elements
+
+    public Lista() {
+        this(CAPACITY);
+    } // constructs list with default capacity
+
+    public Lista(int capacity) { // constructs list with given capacity
+        data = (Nodo[]) new Object[capacity]; // safe cast; compiler may give warning
+    }
+
+    public void add(Nodo nodo) {
+        data[size] = nodo;
+        size++;
+    }
+
+
 }
